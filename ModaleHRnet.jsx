66648e287ddfe64cd143d016f97a081abca441d5
baya laadjel <bata_cher@hotmail.fr>
2024-01-19 15:41:58 +0100

    change with css

--- ModaleHRnet.jsx
+++ ModaleHRnet.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom'
+import './ModaleHRnet.css'
+import { NavLink } from 'react-router-dom'
 
 const  ModaleHRnet = ({closeMoldal,textModal, linkModal, linkTo})=> {
+
   return (
-    <div className='flex justify-center items-center '>
-      <div className='bg-slate-100 opacity-80 w-screen h-screen fixed  top-0 right-0 '>  
+    <div className='ModaleContainer'>
+      <div className='GreyBackgroung'>  
       </div>
-      <div className='relative -top-64 m-3 z-10 p-5 rounded-lg border shadow-lg bg-white w-2/5'> 
-          <button className='absolute -top-2 -right-2 ' onClick={() => { closeMoldal(false)} }> 
-            <FontAwesomeIcon className='w-8 h-8' icon={faCircleXmark} />
+      <div className='MyModale'> 
+          <button className='CloseButton' onClick={() => { closeMoldal(false)} }> 
+            <FontAwesomeIcon className='CloseIcon' icon={faCircleXmark} />
           </button>
-          <p className='font-bold text-2xl p-3'> {textModal} </p>
-          <Link className='p-3 justify-center flex text-emerald-800 font-bold ' to={linkTo}> {linkModal} </Link>
+          <p className='TextOfModal'> {textModal} </p>
+          <NavLink className='LinkOfModal' to={linkTo}> {linkModal} </NavLink>
       </div>
     </div>
   )
